29bf6899c32bd7a53ff51b5baaf413d00b231062
ultram4rine <ultramarine730@gmail.com>
2021-06-16 23:39:06 +0400

    used handlebars for templating

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ mod server;
 extern crate dotenv;
 
 use std::env;
+use std::fs;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
 use dotenv::dotenv;
+use handlebars::{handlebars_helper, Handlebars};
 use telegram_bot::{Api, Error};
 use warp::Filter;
 
@@ -19,11 +21,20 @@ async fn main() -> Result<(), Error> {
     let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
     let bot = Arc::new(Mutex::new(Api::new(token)));
 
+    let tpl_str = fs::read_to_string("templates/default.hbs").unwrap();
+    let mut hb = Handlebars::new();
+    hb.register_template_string("default", tpl_str).unwrap();
+    handlebars_helper!(eq: |x: str, { compare: str = "firing" }| x == compare);
+    hb.register_helper("eq", Box::new(eq));
+
+    let hb = Arc::new(hb);
+
     warp::serve(
         warp::path::end()
             .and(warp::post())
             .and(warp::body::json())
             .and(warp::any().map(move || bot.clone()))
+            .and(warp::any().map(move || hb.clone()))
             .and_then(send_message),
     )
     .run(([0, 0, 0, 0], 3030))
--- src/server.rs
+++ src/server.rs
@@ -1,19 +1,20 @@
 use std::{convert::Infallible, sync::Arc};
 
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+use handlebars::Handlebars;
 use telegram_bot::{Api, ChatId, ParseMode, SendMessage};
 use tokio::sync::Mutex;
 use warp::http::StatusCode;
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 pub struct WebHook {
     pub status: String,
     pub alerts: Vec<Alert>,
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Alert {
     pub labels: Value,
@@ -25,10 +26,17 @@ pub struct Alert {
 pub async fn send_message(
     webhook: WebHook,
     bot: Arc<Mutex<Api>>,
+    hb: Arc<Handlebars<'_>>,
 ) -> Result<impl warp::Reply, Infallible> {
     let b = bot.lock().await;
     match b
-        .send(SendMessage::new(ChatId::from(101814676), webhook.status).parse_mode(ParseMode::Html))
+        .send(
+            SendMessage::new(
+                ChatId::from(101814676),
+                hb.render("default", &webhook).unwrap(),
+            )
+            .parse_mode(ParseMode::Html),
+        )
         .await
     {
         Ok(_) => return Ok(StatusCode::OK),
