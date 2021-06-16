d25be2e3d9545c0f03e6a9e43794440a7a5b9d25
ultram4rine <ultramarine730@gmail.com>
2021-06-17 01:00:29 +0400

    created duration helpers (TODO: human readable duration)

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ extern crate dotenv;
 use std::env;
 use std::fs;
 use std::sync::Arc;
-use tokio::sync::Mutex;
 
+use chrono::prelude::*;
 use dotenv::dotenv;
 use handlebars::{handlebars_helper, Handlebars};
 use telegram_bot::{Api, Error};
+use tokio::sync::Mutex;
 use warp::Filter;
 
 use crate::server::send_message;
@@ -25,7 +26,20 @@ async fn main() -> Result<(), Error> {
     let mut hb = Handlebars::new();
     hb.register_template_string("default", tpl_str).unwrap();
     handlebars_helper!(eq: |x: str, { compare: str = "firing" }| x == compare);
+    handlebars_helper!(since:|x:str| {
+        let time=DateTime::parse_from_rfc3339(x).unwrap();
+        let now=Local::now();
+        format!("{}", now.signed_duration_since(time).num_days())
+    });
+    handlebars_helper!(duration:|x:str, {y: str = ""}| {
+        let from=DateTime::parse_from_rfc3339(x).unwrap();
+        println!("{}", y);
+        let to=DateTime::parse_from_rfc3339(y).unwrap();
+        format!("{}", to.signed_duration_since(from).num_days())
+    });
     hb.register_helper("eq", Box::new(eq));
+    hb.register_helper("since", Box::new(since));
+    hb.register_helper("duration", Box::new(duration));
 
     let hb = Arc::new(hb);
 
