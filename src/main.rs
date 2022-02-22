a939d35c3732c69cc752a7fe9f5fad8048164c1f
ultram4rine <ultramarine730@gmail.com>
2022-02-22 18:39:00 +0400

    use clap

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,11 @@ mod server;
 
 extern crate dotenv;
 
-use std::{env, fs, sync::Arc};
+use std::{fs, sync::Arc};
 
-use anyhow::{anyhow, Result};
+use anyhow::Result;
 use chrono::{DateTime, Local};
+use clap::Parser;
 use dotenv::dotenv;
 use handlebars::{handlebars_helper, Handlebars};
 use teloxide::Bot;
@@ -15,36 +16,42 @@ use warp::Filter;
 use crate::duration::format_duration;
 use crate::server::send_message;
 
+#[derive(Parser, Debug)]
+#[clap(version, about, long_about = None)]
+struct Args {
+    #[clap(
+        short,
+        long,
+        env = "ALERTER_LISTEN_PORT",
+        default_value_t = 48655,
+        help = "Port to listen."
+    )]
+    port: u16,
+
+    #[clap(
+        long,
+        env = "ALERTER_TMPL_PATH",
+        default_value = "templates/default.hbs",
+        help = "Path to handlebars template file."
+    )]
+    template_path: String,
+
+    #[clap(long, env = "ALERTER_TG_BOT_TOKEN", help = "Telegram bot token.")]
+    token: String,
+
+    #[clap(long, env = "ALERTER_TG_CHAT_ID", help = "Telegram chat ID.")]
+    chat_id: i64,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+    let args = Args::parse();
 
-    let listen_port = match env::var("ALERTER_LISTEN_PORT")
-        .expect("LISTEN_PORT not set")
-        .parse::<u16>()
-    {
-        Ok(v) => v,
-        Err(err) => {
-            return Err(anyhow!(format!(
-                "failed to parse LISTEN_PORT to integer: {}",
-                err
-            )));
-        }
-    };
-    let tmpl_path = env::var("ALERTER_TMPL_PATH").expect("TMPL_PATH not set");
-    let token = env::var("ALERTER_TG_BOT_TOKEN").expect("TG_BOT_TOKEN not set");
-    let chat_id = match env::var("ALERTER_TG_CHAT_ID")
-        .expect("TG_CHAT_ID not set")
-        .parse::<i64>()
-    {
-        Ok(v) => v,
-        Err(err) => {
-            return Err(anyhow!(format!(
-                "failed to parse TG_CHAT_ID to integer: {}",
-                err
-            )));
-        }
-    };
+    let listen_port = args.port;
+    let tmpl_path = args.template_path;
+    let token = args.token;
+    let chat_id = args.chat_id;
 
     let bot = Bot::new(token);
 
