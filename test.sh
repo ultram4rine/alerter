2c147d125329d175183279f9949b48e155c56bb9
ultram4rine <ultramarine730@gmail.com>
2022-03-12 15:48:22 +0400

    add argument to tesh script

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-curl \
+curl -i \
     -H "Content-Type: application/json" \
     --request POST \
     --data '{"receiver":"telegram","status":"firing","alerts":[{"status":"firing","labels":{"alertname":"Fire","severity":"critical"},"annotations":{"summary":"Something is on fire"},"startsAt":"2018-11-04T22:43:58.283995108+01:00","endsAt":"2018-11-04T22:46:58.283995108+01:00","generatorURL":"http://localhost:9090/graph?g0.expr=vector%28666%29\u0026g0.tab=1"},{"status":"resolved","labels":{"alertname":"Green","severity":"critical"},"annotations":{"summary":"Something cool! <3"},"startsAt":"2018-11-04T22:43:58.283995108+01:00","endsAt":"2018-11-04T22:46:58.283995108+01:00","generatorURL":"http://localhost:9090/graph?g0.expr=vector%28666%29\u0026g0.tab=1"}],"groupLabels":{"alertname":"Fire"},"commonLabels":{"alertname":"Fire","severity":"critical"},"commonAnnotations":{"message":"Something is on fire"},"externalURL":"http://localhost:9093","version":"4","groupKey":"{}:{alertname=\"Fire\"}"}' \
-    localhost:48655
+    localhost:48655/$1
